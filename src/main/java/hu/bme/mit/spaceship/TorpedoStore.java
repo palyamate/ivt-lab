05461d1ead3f7e5eaa9cc2bac473822f2a684809
matt180 <mpalya98@gmail.com>
2020-05-07 16:49:29 +0200

    Rename the random generator variable.

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,7 +12,7 @@ public class TorpedoStore {
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
-  private Random generator = new Random();
+  private Random randomNumberGenerator = new Random();
 
   private int torpedoCount = 0;
 
@@ -38,7 +38,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    double r = this.generator.nextDouble();
+    double r = this.randomNumberGenerator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
